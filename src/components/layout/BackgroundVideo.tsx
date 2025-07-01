c9ea1a88061d3852c7f0f1a17f9291e6fd47e746
VNDR <support@vndrmusic.com>
2025-07-01 05:49:09 +0000

    use this as the background video https://videos.pexels.com/video-files/4

--- src/components/layout/BackgroundVideo.tsx
+++ src/components/layout/BackgroundVideo.tsx
@@ -9,7 +9,7 @@ export function BackgroundVideo() {
       playsInline
       className="fixed inset-0 w-full h-full object-cover -z-10"
     >
-      <source src="https://firebasestorage.googleapis.com/v0/b/vsd-network.appspot.com/o/files%2Fvideos%2FAdobeStock_552485501_Video_HD_Preview.mov?alt=media&token=c59740fe-471a-45c3-a5c3-6e3e78a63cfc" type="video/mp4" />
+      <source src="https://videos.pexels.com/video-files/4431790/4431790-uhd_2560_1440_30fps.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   );
