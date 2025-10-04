d2d70a3ce3f53e8dd1aaaf90210fac7d0d3104a6
VNDR <support@vndrmusic.com>
2025-10-04 02:57:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "vsd-network",
+  "appId": "1:81625826713:web:9b82f5ed5b766e028a7f08",
+  "apiKey": "AIzaSyDI7Wpp_-LCUUHvel3_8PV1Pq9ST-WiUCY",
+  "authDomain": "vsd-network.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "81625826713"
+};
