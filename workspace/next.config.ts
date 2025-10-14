a60bc729b3e1f4d819799019c42f753d13c5c37e
VNDR <support@vndrmusic.com>
2025-10-14 04:14:42 +0000

    Make sure all images are whitelisted

---
+++ workspace/next.config.ts
@@ -0,0 +1,38 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'indiemedia.llc',
+      },
+       {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+       {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
